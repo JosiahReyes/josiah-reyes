82d479389b39969dee779e6b42dd70040ba0a41d
Alex Michael Pagsanhan <pagsanhan2200173@ceu.edu.ph>
2025-04-28 17:49:45 +0800

    Initial Commit

---
+++ app/todos/new/page.js
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma";
+import TodoForm from "../Form";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export default async function NewTodo() {
+    const session = await getServerSession(authOptions);
+    const saveTodo = async (formData) => {
+        "use server";
+        const todo = await prisma.todo.create({
+            data: {
+                task: formData.get('task'),
+                description: formData.get('description'),
+                done: formData.get('done') ? true : false,
+                deadlineAt: formData.get('deadlineAt') ? new Date(formData.get('deadlineAt')) : null,
+                userId: session.user.id,
+            }
+        });
+        redirect(`/todos/${todo.id}`);
+    };
+    return <TodoForm onSubmit={saveTodo} />
+}
